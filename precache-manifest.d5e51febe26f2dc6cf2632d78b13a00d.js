05e618e216ad3c20883a7ff96c4b593398826a87
Naveen Kumar K <knaveenkumar3576@gmail.com>
2020-04-19 16:02:16 -0400

    Updates

---
+++ precache-manifest.d5e51febe26f2dc6cf2632d78b13a00d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61e3fc6db7570cc290479fbd3bfe623c",
+    "url": "/mini-reddit/index.html"
+  },
+  {
+    "revision": "a5f181f88844558bda67",
+    "url": "/mini-reddit/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "b1b3fe6453a5b752993b",
+    "url": "/mini-reddit/static/css/main.e275826e.chunk.css"
+  },
+  {
+    "revision": "a5f181f88844558bda67",
+    "url": "/mini-reddit/static/js/2.b3faf0e5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mini-reddit/static/js/2.b3faf0e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1b3fe6453a5b752993b",
+    "url": "/mini-reddit/static/js/main.6a75e76e.chunk.js"
+  },
+  {
+    "revision": "650f11415d76bc2fe91d",
+    "url": "/mini-reddit/static/js/runtime-main.a832a5db.js"
+  }
+]);
