13d3789859e490dcd001d265596173a77d3caf97
Naveen Kumar K <knaveenkumar3576@gmail.com>
2020-04-19 15:48:17 -0400

    moved home to components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import RedditHome from './containers/RedditHome';
+import RedditHome from './components/RedditHome';
 
 function App() {
   return (
